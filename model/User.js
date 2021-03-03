f255b29c7f79b149119e4b1fa7e72535beb30e22
Yuka TANAKA <seien_0823@icloud.com>
2021-03-03 16:39:17 +0100

    finish iteration 1

---
+++ model/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: String,
+    email: { type: String,
+    unique:true},
+    password: String,
+    role: {
+      type: String, 
+      enum:["admin","user"],
+      default:"user" },
+    avater:{ type: String, 
+            default:"https://res.cloudinary.com/gdaconcept/image/upload/v1614762472/workshop-artistify/default-profile_tbiwcc.jpg"
+  }
+   }
+);
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
