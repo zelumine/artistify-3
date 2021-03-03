841057cbdbe565ee16a5b82b3af7058982c92d25
fmmyriam <benhmyriam@gmail.com>
2021-03-03 17:06:34 +0100

    part 1

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./../model/');
+const User = require('./../model/User');
+const bcrypt = require('bcrypt');
+
+router.get('/signin', (req, res, next) => {
+    res.render('/auth/signin');
+});
+
+router.get('/signup', (req, res, next) => {
+    res.render('/auth/signup');
+});
+
+router.get('/signout', (req, res, next) => {
+    req.session.destroy((error) => {
+        console.log(error);
+        res.redirect('/signin');
+    })
+});
+
+// router.post('/signin', (req, res, next) => {
+//     res.render('auth/signin');
+// });
+
+// router.post('/signup', (req, res, next) => {
+//     res.render('auth/signup');
+// });
